c592a49b2e34df09dec5c825e639a34dbe7ba0fc
Vivek Ponnaiyan <vivek@fitmountain.com>
2015-08-31 12:53:09 -0700

    [vp] Added readme on how to install locally and how to run the test suite.

--- spec/controllers/api/v1/quotes_controller_spec.rb
+++ spec/controllers/api/v1/quotes_controller_spec.rb
@@ -1,12 +1,24 @@
-require 'spec_helper'
+require 'rails_helper'
 
-describe Api::V1::QuotesController do
+RSpec.describe Api::V1::QuotesController, :type=>:controller do
 
   describe "GET 'index'" do
     it "returns http success" do
-      get 'index'
+      setup_1_ticker_1_quote
+      
+      get 'index', :ticker_id=>1, :format=>:json
+      
       response.should be_success
     end
   end
 
+  def setup_1_ticker_1_quote
+    ticker_model = Ticker.create(name:"GOOG")
+    quote = Quote.create ( {:ticker_id => ticker_model.id,
+                           :ticker_symbol => ticker_model.name,
+                           :price => 94.3434,
+                           :pricing_date=> Time.now.to_date})
+    
+  end
+  
 end
