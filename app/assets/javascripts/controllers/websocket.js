888bbc9e0d0bafd309db7f54c1e0490047ee9569
Vivek Ponnaiyan <vivek@fitmountain.com>
2015-09-13 00:25:08 -0700

    [vp] cleaned up websocket.js

--- app/assets/javascripts/controllers/websocket.js
+++ app/assets/javascripts/controllers/websocket.js
@@ -2,30 +2,13 @@ App.WebsocketController = Ember.Controller.extend({
 	_setup: function() {
 		console.log("Websocket controller is started");
 
-		// var client = undefined;
-		
-		// if ((typeof process !== 'undefined') && process.env.REDISTOGO_URL) {
-		// 	var r = require("url").parse(process.env.REDISTOGO_URL);
-		// 	client = require("redis").createClient(r.port, r.hostname);
-		// 	client.auth(r.auth.split(":")[1]);
-		// } else {
-		// 	client = require("redis").createClient();
-		// }
-
-		var port = 20391;
-		
-		// Get
-		console.log("socket port:"+port);
-		
 		websocket = this.websocket = new WebSocket('ws://stockalizer-node.herokuapp.com');
 
 		this.subscribers = [];
-		this.models = [];
 		
 		websocket.onmessage = function(event) {
-			console.log("hungyen bungyed");
 			if (!this.subscribers) { return;}
-			console.log("message recieved by websocket controller,"+ event.data );
+			console.log("message recieved by websocket controller,"+ event.data);
 			var message = JSON.parse(event.data);
 			this.subscribers.forEach(function(callback){callback(message)});
 		}.bind(this);
